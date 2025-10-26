e243ddd122d26e32878e92eb04d2ea666386326e
Will Forbes <wforbes87@gmail.com>
2025-10-25 18:52:00 -0700

    ch5.3 - eslint & prettier setup

--- devflow/eslint.config.mjs
+++ devflow/eslint.config.mjs
@@ -9,6 +9,7 @@ const eslintConfig = defineConfig([
   globalIgnores([
     // Default ignores of eslint-config-next:
     ".next/**",
+    "node_modules/**/*",
     "out/**",
     "build/**",
     "next-env.d.ts",
