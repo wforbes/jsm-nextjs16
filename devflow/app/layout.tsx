4cd7a53405ecb79fab71b7e755a06894d3aa4627
Will Forbes <wforbes87@gmail.com>
2025-10-26 01:19:38 -0700

    ch8.1 - add next-themes, ThemeProvider
    https://jsmastery.com/course/the-ultimate-next-js-15-course/light-dark-theme/set-up-the-themes

--- devflow/app/layout.tsx
+++ devflow/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { ThemeProvider } from "next-themes";
 
 const inter = localFont({
 	src: "./fonts/InterVF.ttf",
@@ -28,8 +29,12 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang="en">
-			<body className={`${inter.className} ${spaceGrotesk.variable} antialiased`}>{children}</body>
+		<html lang="en" suppressHydrationWarning>
+			<body className={`${inter.className} ${spaceGrotesk.variable} antialiased`}>
+				<ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+					{children}
+				</ThemeProvider>
+			</body>
 		</html>
 	);
 }
