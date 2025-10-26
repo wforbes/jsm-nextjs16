eac34defdf781a1e3756e23408376c77aa032338
Will Forbes <wforbes87@gmail.com>
2025-10-26 01:47:12 -0700

    ch8.2 - add shadcn dropdown and test it

--- devflow/app/page.tsx
+++ devflow/app/page.tsx
@@ -1,8 +1,28 @@
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuLabel,
+	DropdownMenuSeparator,
+	DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
 const Home = () => {
 	return (
 		<div>
 			<h1 className="h1-bold">Welcome to Next.js! 👋</h1>
-			<h1 className="font-space-grotesk h1-bold">Welcome to Next.js! 👋</h1>
+			<p>Testing shadcn-ui Dropdown...</p>
+			<DropdownMenu>
+				<DropdownMenuTrigger>Open</DropdownMenuTrigger>
+				<DropdownMenuContent>
+					<DropdownMenuLabel>My Account</DropdownMenuLabel>
+					<DropdownMenuSeparator />
+					<DropdownMenuItem>Profile</DropdownMenuItem>
+					<DropdownMenuItem>Billing</DropdownMenuItem>
+					<DropdownMenuItem>Team</DropdownMenuItem>
+					<DropdownMenuItem>Subscription</DropdownMenuItem>
+				</DropdownMenuContent>
+			</DropdownMenu>
 		</div>
 	);
 };
