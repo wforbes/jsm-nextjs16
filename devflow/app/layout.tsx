b4062a02ccc7513aded6687780629f985f7d6aad
Will Forbes <wforbes87@gmail.com>
2025-10-26 02:22:20 -0700

    ch8.3 - add Navbar component
    https://jsmastery.com/course/the-ultimate-next-js-15-course/light-dark-theme/creating-the-navbar

--- devflow/app/layout.tsx
+++ devflow/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { ThemeProvider } from "next-themes";
+import Navbar from "@/components/navigation/navbar";
 
 const inter = localFont({
 	src: "./fonts/InterVF.ttf",
@@ -32,6 +33,7 @@ export default function RootLayout({
 		<html lang="en" suppressHydrationWarning>
 			<body className={`${inter.className} ${spaceGrotesk.variable} antialiased`}>
 				<ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+					<Navbar />
 					{children}
 				</ThemeProvider>
 			</body>
