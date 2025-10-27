85f8537fda288e4cdb2a550a836d19fb03795a0e
Will Forbes <wforbes87@gmail.com>
2025-10-26 20:43:21 -0700

    ch9.4 - add auth layout & social auth form

--- devflow/app/(auth)/layout.tsx
+++ devflow/app/(auth)/layout.tsx
@@ -1,7 +1,33 @@
 import React from "react";
+import Image from "next/image";
+import SocialAuthForm from "@/components/forms/SocialAuthForm";
 
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
-	return <main>{children}</main>;
+	return (
+		<main className="bg-auth-light dark:bg-auth-dark flex-center min-h-screen bg-cover bg-center bg-no-repeat px-4 py-10">
+			<section className="light-border background-light800_dark200 shadow-light100_dark100 min-w-full rounded-[10px] border px-4 py-10 shadow-md sm:min-w-[520px] sm:px-8">
+				<div className="flex-between gap-2">
+					<div className="space-y-2.5">
+						<h1 className="h2-bold text-dark100_light900">
+							Join DevFlow
+						</h1>
+						<p className="paragraph-regular text-dark500_light400">
+							To get your questions answered
+						</p>
+					</div>
+					<Image
+						src="images/site-logo.svg"
+						alt="DevFlow Logo"
+						width={50}
+						height={50}
+						className="object-contain"
+					/>
+				</div>
+				{children}
+				<SocialAuthForm />
+			</section>
+		</main>
+	);
 };
 
 export default AuthLayout;
