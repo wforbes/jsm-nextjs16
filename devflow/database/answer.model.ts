3e1adf02dcf9f738f860ed5865373707098c1627
Will Forbes <wforbes87@gmail.com>
2025-11-01 19:48:08 -0700

    ch14.7 - add Answer model

---
+++ devflow/database/answer.model.ts
@@ -0,0 +1,28 @@
+import { model, models, Types, Schema } from "mongoose";
+
+export interface IAnswer {
+	author: Types.ObjectId;
+	question: Types.ObjectId;
+	content: string;
+	upvotes: number;
+	downvotes: number;
+}
+
+const AnswerSchema = new Schema(
+	{
+		author: { type: Schema.Types.ObjectId, ref: "User", required: true },
+		question: {
+			type: Schema.Types.ObjectId,
+			ref: "Question",
+			required: true,
+		},
+		content: { type: String, required: true },
+		upvotes: { type: Number, default: 0 },
+		downvotes: { type: Number, default: 0 },
+	},
+	{ timestamps: true }
+);
+
+const Answer = models?.answer || model<IAnswer>("Answer", AnswerSchema);
+
+export default Answer;
