5217e9b490d4f556587a2a4cd492710222dc7a29
Will Forbes <wforbes87@gmail.com>
2025-11-04 07:46:04 -0800

    ch14.16 - add Collection model
    https://jsmastery.com/course/the-ultimate-next-js-15-course/develop-your-backend/create-collection-model

---
+++ devflow/database/collection.model.ts
@@ -0,0 +1,19 @@
+import { model, models, Types, Schema } from "mongoose";
+
+export interface ICollection {
+	author: Types.ObjectId;
+	question: Types.ObjectId;
+}
+
+const CollectionSchema = new Schema(
+	{
+		author: { type: Types.ObjectId, ref: "User", required: true },
+		question: { type: Types.ObjectId, ref: "Question", required: true },
+	},
+	{ timestamps: true }
+);
+
+const Collection =
+	models?.collection || model<ICollection>("Collection", CollectionSchema);
+
+export default Collection;
