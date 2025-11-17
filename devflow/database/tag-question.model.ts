2d977edc558b0cf2f6ea1c0e06df3135fbd6160f
Will Forbes <wforbes87@gmail.com>
2025-11-16 20:53:58 -0800

    fix field name issue on tagquestion model
    fields on tag-question shouldnt contain 'id' in the names

--- devflow/database/tag-question.model.ts
+++ devflow/database/tag-question.model.ts
@@ -1,15 +1,15 @@
 import { model, models, Types, Schema, Document } from "mongoose";
 
 export interface ITagQuestion {
-	tagId: Types.ObjectId;
-	questionId: Types.ObjectId;
+	tag: Types.ObjectId;
+	question: Types.ObjectId;
 }
 export interface ITagQuestionDocument extends ITagQuestion, Document {}
 
 const TagQuestionSchema = new Schema<ITagQuestion>(
 	{
-		tagId: { type: Schema.Types.ObjectId, ref: "Tag", required: true },
-		questionId: {
+		tag: { type: Schema.Types.ObjectId, ref: "Tag", required: true },
+		question: {
 			type: Schema.Types.ObjectId,
 			ref: "Question",
 			required: true,
