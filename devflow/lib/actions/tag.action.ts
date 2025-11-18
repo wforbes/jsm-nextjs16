e31cd843b429c7d9cddb0c46412eac1a77d35746
Will Forbes <wforbes87@gmail.com>
2025-11-17 17:29:53 -0800

    ch22.3 - implement Tags Question action
    - for retrieving a list of questions given a specific tag id

--- devflow/lib/actions/tag.action.ts
+++ devflow/lib/actions/tag.action.ts
@@ -1,7 +1,10 @@
 import { FilterQuery } from "mongoose";
 import handleError from "../handlers/error";
-import { PaginatedSearchParamsSchema } from "../validations";
-import { Tag } from "@/database";
+import {
+	GetTagQuestionsSchema,
+	PaginatedSearchParamsSchema,
+} from "../validations";
+import { Question, Tag } from "@/database";
 import action from "../handlers/action";
 import { getDeviconClassName, getTechDescription } from "../utils";
 import { techIconMap } from "../techIconMap";
@@ -25,10 +28,7 @@ export async function getTags(
 	if (query) {
 		// TODO: enhance search to be able to look through techIconMap and techDescriptionMap
 		//  and match entries based on that text with the corresponding tag name
-		filterQuery.$or = [
-			{ name: { $regex: query, $options: "i" } },
-			{ description: { $regex: query, $options: "i" } },
-		];
+		filterQuery.$or = [{ name: { $regex: query, $options: "i" } }];
 	}
 
 	let sortCriteria = {};
@@ -61,6 +61,63 @@ export async function getTags(
 		return {
 			success: true,
 			data: { tags: JSON.parse(JSON.stringify(tags)), isNext },
+		};
+	} catch (error) {
+		return handleError(error as Error) as ErrorResponse;
+	}
+}
+
+export async function getTagQuestions(
+	params: GetTagQuestionsParams
+): Promise<
+	ActionResponse<{ tag: Tag; questions: Question[]; isNext: boolean }>
+> {
+	const validationResult = await action({
+		params,
+		schema: GetTagQuestionsSchema,
+	});
+	if (validationResult instanceof Error) {
+		return handleError(validationResult) as ErrorResponse;
+	}
+
+	const { page = 1, pageSize = 10, query, tagId } = validationResult.params!;
+	const skip = (Number(page) - 1) * pageSize;
+	const limit = Number(pageSize);
+
+	try {
+		const tag = await Tag.findById(tagId);
+		if (!tag) {
+			throw new Error("Tag not found.");
+		}
+		const filterQuery: FilterQuery<typeof Question> = {
+			tags: { $in: [tagId] },
+		};
+
+		if (query) {
+			filterQuery.title = { $regex: query, $options: "i" };
+		}
+
+		const totalQuestions = await Question.countDocuments(filterQuery);
+
+		const questions = await Question.find(filterQuery)
+			.select(
+				"_id title views answers upvotes downvotes author createdAt"
+			)
+			.populate([
+				{ path: "author", select: "name image" },
+				{ path: "tags", select: "name" },
+			])
+			.skip(skip)
+			.limit(limit);
+		const isNext = totalQuestions > skip + questions.length;
+
+		return {
+			success: true,
+			data: {
+				tag: JSON.parse(JSON.stringify(tag)),
+				questions: JSON.parse(JSON.stringify(questions)),
+				isNext,
+			},
 		};
 	} catch (error) {
 		return handleError(error as Error) as ErrorResponse;
